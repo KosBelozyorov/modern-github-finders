9e4d7f496e34d011bbb75c24d74d0c5e60731674
KosBelozyorov <KosBelozyorov@gmail.com>
2019-12-04 15:32:50 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import User from './components/users/User';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <HashRouter>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert alert={alert} />
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  render={props => (
+                    <>
+                      <Search />
+                      <Users />
+                    </>
+                  )}
+                />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+              </Switch>
+            </div>
+          </div>
+        </HashRouter>
+      </AlertState>
+    </GithubState>
+  );
+};
+
+export default App;
