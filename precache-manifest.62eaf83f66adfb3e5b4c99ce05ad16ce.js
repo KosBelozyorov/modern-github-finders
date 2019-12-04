1e6f12870e8a17edbc59d4adff4d3cc69b55bf4d
KosBelozyorov <KosBelozyorov@gmail.com>
2019-12-04 15:41:56 +0200

    Updates

---
+++ precache-manifest.62eaf83f66adfb3e5b4c99ce05ad16ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418ed452ae53f2f99098f2f2a8fda2dc",
+    "url": "/modern-github-finders/index.html"
+  },
+  {
+    "revision": "9bd873940485daef814f",
+    "url": "/modern-github-finders/static/css/main.e256f8e7.chunk.css"
+  },
+  {
+    "revision": "27ea8a0b6be707075d03",
+    "url": "/modern-github-finders/static/js/2.796aa639.chunk.js"
+  },
+  {
+    "revision": "9bd873940485daef814f",
+    "url": "/modern-github-finders/static/js/main.530b4207.chunk.js"
+  },
+  {
+    "revision": "30b01f2a409e688a9953",
+    "url": "/modern-github-finders/static/js/runtime-main.c070331c.js"
+  }
+]);
